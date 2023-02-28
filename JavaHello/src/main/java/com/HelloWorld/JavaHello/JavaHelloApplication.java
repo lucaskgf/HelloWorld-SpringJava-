e7a1acc75571645f01d8a7e8d3bb9d31cd3b7c91
Lucas Gusmão <luquinhaskgf@gmail.com>
2023-02-28 20:29:54 -0300

    Add files via upload

---
+++ JavaHello/src/main/java/com/HelloWorld/JavaHello/JavaHelloApplication.java
@@ -0,0 +1,13 @@
+package com.HelloWorld.JavaHello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHelloApplication.class, args);
+	}
+
+}
---
+++ JavaHello/src/test/java/com/HelloWorld/JavaHello/JavaHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HelloWorld.JavaHello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
